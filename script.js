7262a61c27177ecedd6da6bdf37ca9a858a608ba
Alex Harshbarger <harshbarger.a@gmail.com>
2022-03-27 07:15:36 -0400

    added files

---
+++ script.js
@@ -0,0 +1,6 @@
+function buidGrid() {
+    for (let i = 0; i <= 16; i++) {
+        const container = document.querySelector('.container');
+        const div = document.createElement('div');
+    }
+}
