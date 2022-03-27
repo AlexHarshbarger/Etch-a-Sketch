226bc3b56b49083f48e38c28aad841206eb8d7cf
Alex Harshbarger <harshbarger.a@gmail.com>
2022-03-27 11:00:57 -0400

    created prompt and build grid based on number

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function buidGrid() {
-    for (let i = 0; i <= 16; i++) {
-        const container = document.querySelector('.container');
-        const div = document.createElement('div');
+const container = document.querySelector('#container')
+const height = document.getElementById('container').clientHeight;
+const width = document.getElementById('container').clientWidth;
+
+buildGrid()
+
+function buildGrid(size) {
+    size = prompt("Enter number 10-100");
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const grid = document.createElement('div')
+            grid.style.height = height/size - 2 + "px";
+            grid.style.width = width/size - 2 + "px";
+            grid.classList.add('grid');
+            container.appendChild(grid)
+        }
     }
 }
