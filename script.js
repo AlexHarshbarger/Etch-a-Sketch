63724dbe803c725298e8a6a7b476f8c62a582599
Alex Harshbarger <harshbarger.a@gmail.com>
2022-03-28 10:14:03 -0400

    black paint added on mouseover

--- script.js
+++ script.js
@@ -2,26 +2,36 @@ const container = document.querySelector('#container')
 const height = document.getElementById('container').clientHeight;
 const width = document.getElementById('container').clientWidth;
 const reset = document.querySelector('#reset');
+const grid = document.querySelector('.grid');
 
-buildGrid()
+buildGrid(10)
+
 
 
 function buildGrid(size) {
     size = prompt("Enter number 10-100");
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            const grid = document.createElement('div')
+            const grid = document.createElement("div")
             grid.style.height = height/size - 2 + "px";
             grid.style.width = width/size - 2 + "px";
             grid.classList.add('grid');
-            container.appendChild(grid)
+            grid.addEventListener('mouseover', colorIn)
+            container.appendChild(grid);
         }
     }
+    return grid;
 }
 
 reset.addEventListener('click', () => {
-    const gridItems = document.querySelectorAll('#container > div');
-    gridItems.forEach((Item) => {
-        Item.style.backgroundColor = 'gray';
-    })
+    window.location.reload();
+    // const gridItems = document.querySelectorAll('#container > div');
+    // gridItems.forEach((Item) => {
+    //     Item.style.backgroundColor = 'white';
+    // })
 });
+
+function colorIn() {
+    div = this;
+    div.style.backgroundColor = "black";
+}
